7fba028f9228ad4003d193faa8a1c5f5e44d682b
name1125 <81159222+name1125@users.noreply.github.com>
2021-04-07 08:58:00 +0900

    Add files via upload

---
+++ Business.java
@@ -0,0 +1,15 @@
+package team4;
+
+import java.util.Scanner;
+
+public class Business {
+	static Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) {
+		
+		Manager mg = new Manager();
+		
+		mg.start();
+		
+	}
+
+}
