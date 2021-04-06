9bf4694b8bc38b785d23ee7c948568277004a7fb
name1125 <81159222+name1125@users.noreply.github.com>
2021-04-07 08:58:43 +0900

    Add files via upload

---
+++ Food.java
@@ -0,0 +1,29 @@
+package team4;
+
+public interface Food {
+	
+	
+	static int Managerpassword = 1234; 
+	
+	
+	default void search() {
+		
+	}
+	
+	default void entering() {
+		
+	}
+	
+	default void delete() {
+		
+	}
+	
+	default void customer() {
+		
+	}
+	
+	default void password() {
+		
+	}
+
+}
